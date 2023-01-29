9d1983eef9b9505921f0ebe905050bfc0986f120
David <david.diaz.tunjano@gmail.com>
2023-01-29 14:38:41 -0500

    feat: add mobile nav bar script

--- 7-Omnifood/js/script.js
+++ 7-Omnifood/js/script.js
@@ -70,3 +70,11 @@ checkFlexGap();
   }
 }
 */
+
+/* MOBILE NAVIGATION LOGIC */
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
